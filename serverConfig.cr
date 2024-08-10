fa29f38e877aa160dba79281ca5066c536a5268b
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-08-09 17:17:01 -0700

    Create serverConfig.cr

---
+++ serverConfig.cr
@@ -0,0 +1,21 @@
+#interacts with system files, running shell commands, or using APIs.
+
+# server_update.cr
+# can run ad-hoc commands on VM
+def run_command(command : String)
+  puts "Running: #{command}"
+  system(command)
+end
+#can run updates on vm
+def update_system
+  run_command("sudo apt-get update")
+  run_command("sudo apt-get upgrade -y")
+end
+#can clean vm
+def clean_system
+  run_command("sudo apt-get autoremove -y")
+  run_command("sudo apt-get clean")
+end
+
+update_system
+clean_system
